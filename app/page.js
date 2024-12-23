7bb5b0e24e2dc35dc5cd637e7b4b70276450a711
Udbhav Ghimire <ghimireudbhav@gmail.com>
2024-12-23 22:32:22 +0545

    height

--- app/page.js
+++ app/page.js
@@ -396,7 +396,7 @@ export default function ExpensesPage() {
             </div>
 
             {/* Search and Upload Section */}
-            <div className="md:flex md:justify-between md:items-center md:mb-6 hidden">
+            <div className="md:flex md:justify-between md:items-center md:mb-6 hidden mt-6">
               <div className="relative md:w-96">
                 <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-gray-500" />
                 <Input
